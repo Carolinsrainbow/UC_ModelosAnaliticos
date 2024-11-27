25607a814381c314bbcf61cc9eb295a1bd945f3c
CarolinsRainbow <60187454+Carolinsrainbow@users.noreply.github.com>
2024-11-26 21:09:02 -0300

    Add files via upload

--- TF1-PresentingBackingProducts.R
+++ TF1-PresentingBackingProducts.R
@@ -27,5 +27,45 @@ library(readxl)
 
 archivo_base <- "/Users/carolinaherreraazolas/Downloads/Presenting_Banking_Products.xlsx"
 
-data <- read_excel(archivo_base)  # Lo usé sólo para ver como venían los datos
+data <- read_excel(archivo_base) 
 print(data)
+
+#Renombramos la columna Monthly Income
+
+data <- data %>%
+  rename(Monthly_Income = `Monthly Income`) 
+
+# Separación de clientes
+
+# Creamos subconjuntos de datos
+
+grupoConPresentacion <- data %>% filter(Presentation == 1)  # Clientes que asistieron a la presentación
+grupoSinPresentacion <- data %>% filter(Presentation == 0)  # Clientes que no asistieron
+
+# Grupo con presentación
+promedios_presentacion <- grupoConPresentacion %>%
+  summarise(
+    Edad_Promedio = mean(Age, na.rm = TRUE),
+    Ingreso_Promedio = mean(Monthly_Income, na.rm = TRUE),
+    Rentabilidad_Promedio = mean(Profit, na.rm = TRUE)
+  )
+
+# Grupo sin presentación
+promedios_no_presentacion <- grupoSinPresentacion %>%
+  summarise(
+    Edad_Promedio = mean(Age, na.rm = TRUE),
+    Ingreso_Promedio = mean(Monthly_Income, na.rm = TRUE),
+    Rentabilidad_Promedio = mean(Profit, na.rm = TRUE)
+  )
+
+# Mostrar resultados
+print("Promedios para clientes que asistieron a la presentación:")
+print(promedios_presentacion)
+
+print("Promedios para clientes que no asistieron a la presentación:")
+print(promedios_no_presentacion)
+
+# PREGUNTA 1: Calcule la rentabilidad promedio de los clientes que vieron y la de los clientes que no vieron la presentación. ¿Es efectiva la presentación? Explique.
+# RESPUESTA 1: Más que a la efectividad de la presentación, la gran diferencia se da en las rentas promedios, 
+# por lo que es muy probable que la proyección de rentabilidad sea inferior por menor ingreso.
+
